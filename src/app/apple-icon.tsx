02135ac9e334f62dfb67539f78053068a995f649
Harshit Nikam <nikamharshit55@gmail.com>
2025-12-07 22:29:34 +0530

    Favicon Bug Fix

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const size = {
+  width: 180,
+  height: 180,
+}
+export const contentType = 'image/png'
+ 
+// Image generation
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 100,
+          background: 'linear-gradient(135deg, #FFAC36 0%, #EA7201 100%)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          fontWeight: 'bold',
+          borderRadius: '22.5%',
+        }}
+      >
+        F
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
